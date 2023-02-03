659342b24b1bb9882e014f9442d5ea5f7041c23c
RIMaker <zhoraagadzhanyan@gmail.com>
2023-02-03 16:20:20 +0300

    initial commit

---
+++ SurfTask/Common/Assemblers/ViewFactory.swift
@@ -0,0 +1,42 @@
+//
+//  ViewFactory.swift
+//  SurfTask
+//
+//  Created by Zhora Agadzhanyan on 03.02.2023.
+//
+
+import UIKit
+
+enum ScreenIdentifier {
+    case mainScreen
+    case launchScreen
+}
+
+protocol ViewFactory {
+    func makeView(for screenIdentifier: ScreenIdentifier) -> UIViewController?
+}
+
+class ViewFactoryImpl: ViewFactory {
+    
+    func makeView(for screenIdentifier: ScreenIdentifier) -> UIViewController? {
+        switch screenIdentifier {
+        case .mainScreen: return makeMainScreen()
+        case .launchScreen: return makeLaunchScreen()
+        }
+    }
+}
+
+// MARK: - Supporting functions
+extension ViewFactoryImpl {
+    func makeMainScreen() -> UIViewController {
+        let mainVC = ViewController()
+        
+        return mainVC
+    }
+    
+    func makeLaunchScreen() -> UIViewController? {
+        let storyboard = UIStoryboard(name: "LaunchScreen", bundle: .main)
+        let launchScreen = storyboard.instantiateInitialViewController()
+        return launchScreen
+    }
+}
